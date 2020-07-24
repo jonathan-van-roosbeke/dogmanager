73acfc03aa0ec99c52c42b52536a5e6c1e40b9bf
Mnechab <mohamednechab.mn@gmail.com>
2020-07-24 15:00:57 +0200

    gestion d'exception inscription back

--- dogmanager-jvr-mn/src/test/java/persistance/UtilisateurDaoTest.java
+++ dogmanager-jvr-mn/src/test/java/persistance/UtilisateurDaoTest.java
@@ -2,6 +2,7 @@ package persistance;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.Alphanumeric;
@@ -11,6 +12,7 @@ import org.springframework.context.ApplicationContext;
 
 import com.dogmanager.bean.Utilisateur;
 import com.dogmanager.dao.IUtilisateurDao;
+import com.dogmanager.dto.RetourService;
 import com.dogmanager.util.ContextConfigurationType;
 import com.dogmanager.util.MyContextFactory;
 
@@ -26,26 +28,27 @@ public class UtilisateurDaoTest {
 	}
 
 	@Test
-	public void ainscription() {
+	public void aInscription() {
 		Utilisateur utilisateur = new Utilisateur();
 		utilisateur.setNom("momo123");
 		utilisateur.setPrenom("momo123");
 		utilisateur.setLogin("momo1235");
-		utilisateur.setPassword("momo123");
-		boolean resultat = utilisateurDao.inscription(utilisateur);
-		assertNotEquals(false, resultat);
+		utilisateur.setPassword("momo1235");
+		RetourService<Utilisateur> resultat = utilisateurDao.inscription(utilisateur);
+		assertTrue(resultat.isReussi());
 	}
 
 	@Test
-	public void connexion() {
-		Utilisateur user = utilisateurDao.connexion("momo12356", "momo123");
+	public void cDelete() {
+		int resultat = utilisateurDao.deleteByLogin("momo1235");
+		assertNotEquals(0, resultat);
+	}
+
+	@Test
+	public void bConnexion() {
+		Utilisateur user = utilisateurDao.connexion("momo1235", "momo1235");
 		assertNotNull(user);
 		assertNotEquals(0, user.getId());
 	}
 
-	@Test
-	public void delete() {
-		int resultat = utilisateurDao.deleteByLogin("momo1235");
-		assertNotEquals(0, resultat);
-	}
 }
